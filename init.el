4b1ff0347f7dfbedf3e81713c2de2efda99a5606
alsfreitas <anderson.freitass@gmail.com>
2017-01-21 02:25:21 -0200

    file init.org renamed to loader.org and init.el no longer replaces itself with the tangled content of init.org. Now the content of loader.org generates the file loader.el which is then byte compiled

--- init.el
+++ init.el
@@ -1,12 +1,10 @@
-;; This file replaces itself with the actual configuration at first run.
-
 ;; We can't tangle without org!
 (require 'org)
 ;; Open the configuration
-(find-file (concat user-emacs-directory "init.org"))
+(find-file (concat user-emacs-directory "loader.org"))
 ;; tangle it
 (org-babel-tangle)
 ;; load it
-(load-file (concat user-emacs-directory "init.el"))
+(load-file (concat user-emacs-directory "loader.el"))
 ;; finally byte-compile it
-(byte-compile-file (concat user-emacs-directory "init.el"))
+(byte-compile-file (concat user-emacs-directory "loader.el"))
